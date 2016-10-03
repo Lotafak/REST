e5906356b8b5eba332e02c246616f7c5eb71e704
patrykk <patrykk.93@gmail.com>
2016-10-03 23:20:14 +0200

    Final version of RESTful API project.

---
+++ src/main/java/pl/patryk/ObjectIdJaxbAdapter.java
@@ -0,0 +1,7 @@
+package pl.patryk;
+
+/**
+ * Created by Pyra on 2016-04-28.
+ */
+public class ObjectIdJaxbAdapter {
+}
---
+++ src/main/java/pl/patryk/StudentRepresentation.java
@@ -0,0 +1,7 @@
+package pl.patryk;
+
+/**
+ * Created by Pyra on 2016-04-24.
+ */
+public class StudentRepresentation {
+}
